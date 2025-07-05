6d096eeedd1811c8f6661ef6f01569b960f66803
AMFarhan21 <AMFarhan2102@gmail.com>
2025-07-05 09:42:11 +0800

    about

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,7 +6,9 @@ const About = () => {
             <div className='font-bold text-2xl sm:text-2xl text-left'>About</div>
             <div className='text-muted-foreground text-sm text-justify'>
                 I'm a passionate Fullstack Developer with a strong interest in building things. With hands-on experience in technologies like React, Next.js, and React Native, I enjoy crafting seamless web and mobile applications from front to back.
+                <br /><br />
                 Programming is my hobby. I find joy in learning new tools, refining my skills, and tackling challenges that help me grow as a developer. Whether it's experimenting with new frameworks or optimizing existing solutions, I'm always driven by curiosity and a desire to improve.
+                <br /><br />
                 My focus is on continuous improvement and pushing my limits as a developer through hands-on projects and real-world problem solving.                </div>
         </div>
     )
